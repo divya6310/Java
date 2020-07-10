0191207ba71df4f1b939cab5e754dc8e50439750
Geek <a.divy6310@gmail.com>
2020-07-10 19:54:14 +0530

    CountWordsHashMap

---
+++ src/main/java/gitJenkin/CountWordsHashMap.java
@@ -0,0 +1,29 @@
+package gitJenkin;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class CountWordsHashMap extends Util{
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Object[] manip = readData(1);
+		for (int i=0;i<=manip.length-1;i++)
+		{
+			String[] sen=manip[i].toString().split(" ");
+			HashMap<Integer,String> map = new HashMap<Integer,String>();
+			for(int j=0;j<=sen.length-1;j++)
+			{
+				map.put(j, sen[j]);
+			}
+			System.out.println(map.size());
+			sheet.getRow(i+1).createCell(3).setCellValue(map.size());
+		}
+		fis.close();
+		writeData();
+		
+		
+
+	}
+
+}
