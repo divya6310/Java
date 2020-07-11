83a8d8ad2c610b642737e0812c413d3ab28585a1
Geek <a.divy6310@gmail.com>
2020-07-11 22:47:41 +0530

    Iterate while for each

---
+++ src/main/java/gitJenkin/IterateWhile.java
@@ -0,0 +1,34 @@
+package gitJenkin;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class IterateWhile extends CountWordsHashMap{
+
+	public static void main(String[] args) {
+		
+		HashMap<Integer,String> map=new HashMap<Integer,String>();
+		map.put(1,"Selenium");
+		map.put(2, "TestNG");
+		map.put(3, "Git");
+		
+		System.out.println(map);
+		Iterator<Entry<Integer, String>> iterator = map.entrySet().iterator();
+		
+		while(iterator.hasNext())
+		{
+			Map.Entry me =(Map.Entry) iterator.next();
+			System.out.println(me.getKey()+" = "+ me.getValue());
+			
+		}
+		
+		for (Map.Entry me1: map.entrySet())
+		{
+			System.out.println(me1.getValue() +" = "+ me1.getKey());
+		}
+
+	}
+
+}
